52fd836c9f8d1d59322a10a76625b28168c90585
Andrew Watkins <watts@influxdb.com>
2018-09-19 15:16:50 -0700

    Rename files for proper ordering

--- chapter_1/1.03.rkt
+++ chapter_1/1.03.rkt
@@ -11,8 +11,8 @@
 
 (define (howdy a b c)
     (cond 
-        ((and (>= a b) (>= c b)) (sum-of-squares a c))
-        ((and (>= b a) (>= c a)) (sum-of-squares b c))
-        (else sum-of-squares(b a))))
+        ((and (> a b) (> c b)) (sum-of-squares a c))
+        ((and (> b a) (> c a)) (sum-of-squares b c))
+        (else (sum-of-squares b a))))
 
-(howdy 1 1 2)
+(howdy 2 1 1)
