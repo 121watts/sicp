3c16e0378bb624b4d5458cd05f9e1500e3975756
Andrew Watkins <watts@influxdb.com>
2018-09-20 09:04:04 -0700

    Exercise 1.07.rkt

---
+++ chapter_1/1.07.rkt
@@ -0,0 +1,5 @@
+; The good-enough? test used in computing square roots will not be very effective for finding the square roots of very small numbers.
+; Also, in real computers, arithmetic operations are almost always performed with limited precision. This makes our test inadequate for very large numbers.
+; Explain these statements
+; 1) For small numbers, the precision of .001 might be too large
+; 2) For large numbers the precision of .001 might be too small
